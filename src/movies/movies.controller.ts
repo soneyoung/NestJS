54c3705b4ec519cf992a49a1112877b14ffcd189
Juno-Son <sonhy825>
2021-03-17 14:52:14 +0900

    Movie Rest API

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,37 +1,38 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import { Movie } from './entities/movie.entity';
+import { MoviesService } from './movies.service';
+
 
 @Controller('movies')
 export class MoviesController {
+    constructor(private readonly moviesService: MoviesService){}
+    
     @Get()
-    getAll(){
-        return 'This will return all movies';
+    getAll(): Movie[] {
+        return this.moviesService.getAll();
     }
-    @Get("search")  // search 가 get 보다 아래에 있으면 search 를 id로 판단 -> 위로 올려버리기
-    search(@Query("year") searchingYear : string ){
-        return `We are searching for a movie made after: ${searchingYear} `;   //http://localhost:3000/movies/search?year=2000
-    }
+    // @Get("search")  // search 가 get 보다 아래에 있으면 search 를 id로 판단 -> 위로 올려버리기
+    // search(@Query("year") searchingYear : string ){
+    //     return `We are searching for a movie made after: ${searchingYear} `;   //http://localhost:3000/movies/search?year=2000
+    // }
 
     @Get('/:id')    // http://localhost:3000/movies/1    요청하기
-    getOne(@Param('id') movieId: string) {   // param 뒤에 id 10번 라인 id랑 같아야 함
-        return `This will return one movie with the id: ${movieId}`;
+    getOne(@Param('id') movieId: string): Movie {   // param 뒤에 id 10번 라인 id랑 같아야 함
+        return this.moviesService.getOne(movieId);
       }
     @Post()
-    create(@Body() movieData){
-        console.log(movieData)
-        return movieData;
+    create(@Body() movieData){        
+        return  this.moviesService.create(movieData);
     }
 
     @Delete('/:id')
     remove(@Param('id') movieId: string){
-        return `This will delete a movie with the id: ${movieId}`;  
+        return this.moviesService.deleteOne(movieId);
     }  
     
     @Patch('/:id')
     patch(@Param('id') movieId: string, @Body() updateData) {
-        return {
-            updatedMovie: movieId,
-            ...updateData,
-        };
+        return this.moviesService.update(movieId, updateData);
   }
 
 
